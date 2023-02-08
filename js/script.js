843946af39417c63c6ac85f5151d2f0bf35bf5bc
Shirley Ramos Dos Santos Andrade <shirley.andrade@akadseguros.com.br>
2023-02-08 20:02:05 -0300

    incluindo js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+}
